99d18a3c5007bb9b28f39642b0e60677d0a452ec
Kumar Abhinav <kmrabhinav25@gmail.com>
2024-06-20 15:46:49 +0530

    Implement reverse-nodes-in-k-group for linked list

--- leet_practice/sol_93/main.c
+++ leet_practice/sol_93/main.c
@@ -1,22 +1,101 @@
 // https://leetcode.com/problems/reverse-nodes-in-k-group/
-#include "linked_list.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main()
-{
+struct ListNode {
+  int val;
+  struct ListNode *next;
+ };
 
-  Node* l1 = create_list(7);
- add_element(l1, 6, -1);
- add_element(l1, 6, -1);
-//  add_element(l1, 7, -1);
-//  add_element(l1, 7, -1);
-//  add_element(l1, 7, -1);
-//  add_element(l1, 7, -1);
-//  add_element(l1, 7, -1);
+// Linked List functions: 
+struct ListNode* newNode(){
+  struct ListNode* node = (struct ListNode*)malloc(sizeof(struct ListNode*));
+  return node;
+}
 
-//  print_list(l1);
-//  print_list(rr);
-Node* half = reverseKGroup(l1, 7);
- print_list(half);
+void printList(struct ListNode* head) {
+  struct ListNode* tmp = head;
+  
+  if (tmp == NULL) {
+    printf("No item in List");
+    return;
+  }
+  while(tmp != NULL) {
+    printf("%d\n", tmp->val);
+    tmp = tmp->next;
+  }
+}
+
+void insert(struct ListNode* head, int element) {
+  struct ListNode* tmp = head;
+  while (tmp->next != NULL) {
+    tmp = tmp->next;
+  }
+  
+  struct ListNode* node = newNode();
+  node->val = element;
+  node->next = NULL;
+  tmp->next = node;
+}
+
+struct ListNode* createHead(int element) {
+  struct ListNode* node = newNode();
+  node->val = element;
+  node->next = NULL;
+  return node;
+}
+
+struct ListNode* reverseList(struct ListNode *head) {
+    if(!head || !(head->next)) return head;
+    struct ListNode *res = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return res;
+}
+
+struct ListNode* reverseGroup(struct ListNode* head, int k) {
+  if (head == NULL) {
+    return head;
+  }
+  int i = 0;
+  struct ListNode* tmp = head;
+
+  while (i < k && tmp->next != NULL ) {
+    tmp = tmp->next;
+    ++i;
+  }
+  struct ListNode* lastNode = reverseGroup(tmp->next, k);
+  if (i == k) {
+    tmp->next = NULL;
+    struct ListNode* reversedHead = reverseList(head);
+    head->next = lastNode;
+    return reversedHead;
+  }
+  else {
+    return head;
+  }
+}
+
+struct ListNode* reverseKGroup(struct ListNode* head, int k) {
+  struct ListNode* tmp = reverseGroup(head, k-1);
+  return tmp;
+}
+
+int main() {
+
+  struct ListNode *head = createHead(1);
+  insert(head, 2);
+  insert(head, 3);
+  insert(head, 4);
+  insert(head, 5);
+  insert(head, 6);
+  insert(head, 7);
+  insert(head, 8);
+
+
+  struct ListNode *head1 = reverseKGroup(head, 5);
+  printList(head1);
 
  return 0;
 }
